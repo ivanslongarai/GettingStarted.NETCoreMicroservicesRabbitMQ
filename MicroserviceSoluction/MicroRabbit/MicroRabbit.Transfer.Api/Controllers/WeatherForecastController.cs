379e7d4c0f291f4b7c5d8b69d9bc5cc6a7e4cfda
Ivan Longarai <ivanslongarai@gmail.com>
2021-05-21 18:54:27 -0300

    Initial Commit of Transfer Microservice Projects

---
+++ MicroserviceSoluction/MicroRabbit/MicroRabbit.Transfer.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroRabbit.Transfer.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };      
+    }
+}
